8cb401fb4296429ef07085960c22c01fe0dbf4f7
vijendrapatel <vijendra.patel.we2code@gmail.com>
2022-10-03 22:58:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-        <Wave text="Dekha.. Maine Project Deploy kr diya" />
+        <Wave text="shivani tu sach me bhot acchi LADKI hai..." />
         </p>
         <p>
         {/* <Wave text="Dekha.. Maine Project Deploy kr diya" />
